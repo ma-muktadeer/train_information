61225969e6c47490d1c2d40e5fac4dcb239d1241
ma-muktadeer <muktadeer.ma@gmail.com>
2025-07-02 22:16:01 +0600

    login

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'login',
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: 'train-matrix',
     renderMode: RenderMode.Prerender,
   },
