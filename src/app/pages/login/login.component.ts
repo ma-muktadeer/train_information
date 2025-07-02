5b1bfb9d1fefcd91f9676af240b936f566043421
ma-muktadeer <muktadeer.ma@gmail.com>
2025-07-02 23:10:12 +0600

    login ui ok

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,28 +1,21 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-import { Router, RouterModule } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [
-    CommonModule,
     ReactiveFormsModule,
-    RouterModule,
-    MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
     MatButtonModule,
-    MatDividerModule,
-    MatSnackBarModule],
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
