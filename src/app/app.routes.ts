c3b9f5f4eafe2ab1b6a407a57dc6a4a85ef7d9e0
ma-muktadeer <muktadeer.ma@gmail.com>
2025-07-11 03:14:16 +0600

    router ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -60,7 +60,7 @@ export const routes: Routes = [
     }),
   },
   {
-    path: '**', redirectTo: 'train-matrix', pathMatch: 'full'
+    path: '**', redirectTo: 'login', pathMatch: 'full'
   }
 
 ];
